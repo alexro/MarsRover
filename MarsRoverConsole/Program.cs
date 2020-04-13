db173dd68b883ad08d29854516f08751a0a15e7d
alexro <alexro@users.noreply.github.com>
2020-04-13 11:14:46 +0000

    Update Program.cs

--- MarsRoverConsole/Program.cs
+++ MarsRoverConsole/Program.cs
@@ -16,7 +16,47 @@ LMLMLMLMM
 MMRMMRMRRM
 "
                 );
-            Console.ReadLine();
+            Console.WriteLine(@"
+Please enter Are, Rover position and Moves using this format. 
+
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+
+Then enter 'run' on another line to execute. 
+Enter 'end' to exit.
+");
+
+            var isActive = true;
+            var input = string.Empty;
+            do
+            {
+
+                var line = Console.ReadLine().Trim();
+                if (line == "end")
+                {
+                    isActive = false;
+                }
+                else if (line == "run")
+                {
+                    try
+                    {
+                        e.Execute(input);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.WriteLine("Please try again with correct input");
+                    }
+                    input = string.Empty;
+                }
+                else
+                {
+                    input += line + Environment.NewLine;
+                }
+            } while (isActive);
         }
     }
 }
